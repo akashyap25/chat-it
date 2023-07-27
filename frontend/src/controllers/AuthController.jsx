a8a21b3f3ce8e56630646799ff0263b2b7a62f08
akashyap25 <anuragkashyap026@gmail.com>
2023-07-27 09:38:30 +0530

    improved auth part

---
+++ frontend/src/controllers/AuthController.jsx
@@ -0,0 +1,42 @@
+import { createContext, useReducer, useEffect } from "react";
+import { Type } from "../types";
+
+const AuthContext = createContext(null);
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case Type.LOGOUT:
+            return {
+                user: null
+            };
+        case Type.LOGIN:
+            return {
+                user: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+const initialState = {
+    user: null
+};
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    useEffect(() => {
+        const user = localStorage.getItem("user");
+        if (user) {
+            dispatch({ type: Type.LOGIN, payload: JSON.parse(user) });
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
